c3b52cae92583f177009817028e3bbf19c898f3a
Quang <thanhquangtran307@gmail.com>
2022-10-07 20:10:55 +0700

    update code

---
+++ js/Bai1.js
@@ -0,0 +1,20 @@
+/**
+ * Tính tiền lương nhân viên
+ * Khối 1: Input
+ *      Nhập dữ liệu số ngày làm
+ * 
+ * Khối 2: Xử lý
+ *  B1: Khởi tạo biến số ngày soNgay
+ *  B2: Lập công thức
+ *      Luong = soNgay * 100000;
+ *  B3: Thông báo kết quả (console.log)
+ * 
+ * Khối 3: Output
+ *  Số lương nhận được 
+ * 
+ */
+
+var soNgay = 0;
+
+var luong = 0;
+
